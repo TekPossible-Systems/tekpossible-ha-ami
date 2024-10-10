c777acf832a8a2ddaf5a1a403c6c2af2434e466d
Griffin Kiesecker <gkiesecker@wks01.tekp.dev>
2024-10-09 23:42:14 -0600

    Starting work on the pre deployment side of the image. I really want to see if the whole system is going to work as I am very excited to see it do cool stuff

---
+++ setup_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Prep work for RHEL 9 STIG Role From DISA. Note: I did not modify  the DISA role so it should be modular for when there is an update to it.
+cd ansible
+sed -i 's/do_reboot/do_notify_reboot/g' roles/rhel9STIG/tasks/main.yml
+echo "- name: do_notify_reboot\n  debug:\n    msg: 'AMI does not need reboot.'" >> ./roles/rhel9STIG/handlers/main.yml
+
+# Run ansible local setup
+ansible-playbook playbooks/site.yml -c local -vv -i inventory/localsetup
