d7c146b7d33752230c0b8c506ab40f7cb1357113
Griffin Kiesecker <gkiesecker@wks01.tekp.dev>
2024-10-14 22:07:59 -0600

    Fixed some errors and added some more log statements. Given my testing, I will need to complete the git stuff in my execute_imagebuilder.sh script

--- execute_imagebuilder.sh
+++ execute_imagebuilder.sh
@@ -5,10 +5,12 @@ export CDK_STACK_BASE_NAME_LOWER="tekpossible-stratagem"
 export IMAGE_PIPELINE_ARN=$(aws ssm get-parameter --name $CDK_STACK_BASE_NAME_LOWER-imagebuilder-arn --query 'Parameter.Value' | sed 's/"//g')
 export IMAGE_VERSION_ARN=$(aws imagebuilder start-image-pipeline-execution --image-pipeline-arn $IMAGE_PIPELINE_ARN --query 'imageBuildVersionArn' | sed 's/"//g')
 export IMAGE_STATE=$(aws imagebuilder get-image --image-build-version-arn $IMAGE_VERSION_ARN --query 'image.state.status'  | sed 's/"//g')
+echo "The Image Builder Pipeline started. Please go to the imagebuilder pipeline console if you wish to see more about this build"
+echo "The image version ARN is $IMAGE_VERSION_ARN"
 
-while ['$IMAGE_STATE' not "AVAILABLE"]
+while [ "$IMAGE_STATE" != "AVAILABLE" ]
 do 
-	if [ '$IMAGE_STATE' = 'FAILED' ]; then 
+	if [ "$IMAGE_STATE" = 'FAILED' ]; then 
 		echo "Failure in image builder pipeline. Please look at the image builder logs"  
 		exit 1 
 	fi
@@ -17,3 +19,4 @@ done
 
 echo "Image builder succeeded. Working on commiting the AMI ID to the infrastructure repo..."
 export AMI_IMAGE_ID=$(aws ec2 describe-images --filters "Name=tag:Ec2ImageBuilderArn,Values=$IMAGE_VERSION_ARN" --query 'Images[0].ImageId' | sed 's/"//g')
+echo "The following AMI ID will be used: $AMI_IMAGE_ID"
