a47cfed8c44855ca3d595d6d2583560688e407ec
Griffin Kiesecker <gkiesecker@wks01.tekp.dev>
2024-10-18 22:07:26 -0600

    fixed some of the syntax issues we would have had. Also fixed the inventory up a little

--- ansible_set.py
+++ ansible_set.py
@@ -8,6 +8,6 @@ value = str(sys.argv[2])
 
 with open('./ansible/inventory/localsetup/hosts.yml', 'r') as hosts_file:
     hosts_ansible = yaml.safe_load(hosts_file)
-    hosts_ansible['localhost'][key] = value
+    hosts_ansible['all']['hosts']['localhost'][key] = value
     with open('./ansible/inventory/localsetup/hosts.yml', 'w') as write_hosts:
         yaml.dump(hosts_ansible, write_hosts)
--- setup_image.sh
+++ setup_image.sh
@@ -2,8 +2,6 @@
 
 # Prep work for RHEL 9 STIG Role From DISA. Note: I did not modify  the DISA role so it should be modular for when there is an update to it.
 cd ansible
-sed -i 's/do_reboot/do_notify_reboot/g' roles/rhel9STIG/tasks/main.yml
-echo "- name: do_notify_reboot\n  debug:\n    msg: 'AMI does not need reboot.'" >> ./roles/rhel9STIG/handlers/main.yml
 
 # Run ansible local setup
 ansible-galaxy collection install community.general ansible.posix
